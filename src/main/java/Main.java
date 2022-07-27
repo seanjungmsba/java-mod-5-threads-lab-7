0f0e5e81a94c5f76fe0cd118fe0154bee83ab85b
seanjungmsba <seanjeong1996@gmail.com>
2022-07-27 12:20:40 -0400

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,9 @@ import java.util.concurrent.*;
 
 public class Main  {
 
-    private static int num;
-
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
-
         Future<Integer> integerFuture = singleThreadExecutor.submit(getCallableInteger());
-
         System.out.println("Getting value: " + integerFuture.get());
         System.out.println("Is it done? " + integerFuture.isDone());
         System.out.println("======= Shutting Down Executor =======");
@@ -20,14 +16,12 @@ public class Main  {
     public static Callable<Integer> getCallableInteger() {
         // your code here
         Scanner sc = new Scanner(System.in);
-
         Callable<Integer> callable = () -> {
             System.out.println(Thread.currentThread().getName() + " is responsible for this call");
             System.out.print("Enter a number to store: ");
             int numberToStore = sc.nextInt();
             return numberToStore;
         };
-
         return callable;
     }
 
