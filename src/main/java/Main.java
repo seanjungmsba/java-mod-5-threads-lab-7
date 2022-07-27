4561cfa8833376481577f2eb73570348dfcf253b
seanjungmsba <seanjeong1996@gmail.com>
2022-07-26 20:14:42 -0400

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,29 @@ import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 public class Main {
+
+    private static int num;
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter num: ");
+            num = scanner.nextInt();
+            System.out.println(getCallableInteger());
+        }
+
+    }
     public static Callable<Integer> getCallableInteger() {
         // your code here
+        System.out.println("STATUS: I am in getCallableInteger() method");
+        Callable<Integer> callable = () -> {
+            System.out.println("STATUS: I am in call() method");
+            return num;
+        };
+        return callable;
     }
+
 }
